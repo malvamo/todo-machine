3f9687039e6c5581f07c9b863b7c4d27d123139e
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-09-25 16:24:18 -0500

    Implementación de completar y eliminar TODOs

--- src/App.js
+++ src/App.js
@@ -29,7 +29,23 @@ function App() {
     }
   );
 
-  console.log('Los usuarios buscan todos de ' + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
       <>
@@ -46,6 +62,8 @@ function App() {
               key={todo.text}
               text={todo.text} 
               completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
             />
           ))}
         </TodoList>
