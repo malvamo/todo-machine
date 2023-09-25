715545554eda3692979f0ca661ea63e8773c7103
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-09-25 10:49:31 -0500

    Comunicación entre componentes, uso de props y atributos

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
 import './App.css';
+
+
+const defaultTodos = [
+  { text: 'Salir a caminar', completed: false },
+  { text: 'Desayunar', completed: true },
+  { text: 'Estudiar', completed: true },
+  { text: 'Trabajar', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
+      <React.Fragment>
+        <TodoCounter completed={16} total={25}/>
+        <TodoSearch/>
 
-      <TodoCounter/>
-      <TodoSearch/>
+        <TodoList>
+          {defaultTodos.map(todo => (
+            <TodoItem 
+              key={todo.text}
+              text={todo.text} 
+              completed={todo.completed}
+            />
+          ))}
+        </TodoList>
 
-      <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-      </TodoList>
-
-      <CreateTodoButton/>
-
-    </div>
+        <CreateTodoButton/>
+      </React.Fragment>
   );
 }
 
